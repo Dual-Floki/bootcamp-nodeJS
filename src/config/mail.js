92e02938cb499a16ef5461a3fda23b5732e27f98
Dual-Floki <fezto.17@gmail.com>
2019-10-08 20:23:05 -0300

    Enviar email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "8f47c14470442d",
+    pass: "45e226c34bfb98"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
